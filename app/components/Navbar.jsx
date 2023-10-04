cce5f6326d98ba1bb1dd2afd59d411daf6c39e88
pikkauhn <114180449+pikkauhn@users.noreply.github.com>
2023-10-04 16:13:05 -0500

    Navbar changes and page additions

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,37 +1,54 @@
 "use client"
 
+import './navbar.css'
 import { Menubar } from 'primereact/menubar';
 import { InputText } from 'primereact/inputtext';
+
+import Link from 'next/link';
 import Image from 'next/image';
 
 import Logo from '../assets/images/logo.png';
 
+
 export default function Navbar() {
     const items = [
+
+        {
+            label: 'Song Catalog', icon: 'pi pi-fw pi-user', url: 'Songs'
+        },
+        {
+            label: 'Favorites',
+            icon: 'pi pi-fw pi-star',
+            url: 'Favorites'
+        },
         {
             label: 'User', icon: 'pi pi-fw pi-user',
             items: [
                 {
-                    label: 'Favorites',
-                    icon: 'pi pi-fw pi-star'
+                    label: 'Profile',
+                    icon: 'pi pi-fw pi-cog',
+                    url: 'Profile'
                 },
                 {
                     label: 'Settings',
-                    icon: 'pi pi-fw pi-cog'
+                    icon: 'pi pi-fw pi-cog',
+                    url: 'Settings'
                 }
             ]
         }
+
     ]
 
-
-    const start = <Image alt="scaryoke-logo" src={Logo} width="40" className="mr-2" />;
+    const start = <Link href="/">
+        <Image alt="scaryoke-logo" src={Logo} width="40" className="mr-2" />
+    </Link>;
     const end = (
         <span className="p-input-icon-left">
             <i className="pi pi-search" />
             <InputText placeholder="Search" />
         </span>
     );
-    
+
     return (
         <Menubar model={items} start={start} end={end} />
     )
